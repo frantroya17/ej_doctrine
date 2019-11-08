b27a372f069e66e01a00ec7d596c40f63faeadbe
Luis Ramón López López <lrlopez@gmail.com>
2019-11-08 14:19:12 +0100

    Implementada la entidad Profesor

---
+++ src/AppBundle/Entity/Profesor.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="profesor")
+ */
+class Profesor
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $apellidos;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     * @var string
+     */
+    private $dni;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param string $nombre
+     * @return Profesor
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * @param string $apellidos
+     * @return Profesor
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDni()
+    {
+        return $this->dni;
+    }
+
+    /**
+     * @param string $dni
+     * @return Profesor
+     */
+    public function setDni($dni)
+    {
+        $this->dni = $dni;
+        return $this;
+    }
+}
