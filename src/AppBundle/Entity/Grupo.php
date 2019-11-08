c94e344c1e8191136109c360122894a8ac0403eb
Luis Ramón López López <lrlopez@gmail.com>
2019-11-08 14:16:47 +0100

    Añadidos getters/setters de Grupo

--- src/AppBundle/Entity/Grupo.php
+++ src/AppBundle/Entity/Grupo.php
@@ -14,21 +14,87 @@ class Grupo
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
+     * @var int
      */
     private $id;
 
     /**
      * @ORM\Column(type="string")
+     * @var string
      */
     private $descripcion;
 
     /**
      * @ORM\Column(type="integer")
+     * @var int
      */
     private $aula;
 
     /**
      * @ORM\Column(type="integer")
+     * @var int
      */
     private $planta;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * @param string $descripcion
+     * @return Grupo
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAula()
+    {
+        return $this->aula;
+    }
+
+    /**
+     * @param int $aula
+     * @return Grupo
+     */
+    public function setAula($aula)
+    {
+        $this->aula = $aula;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPlanta()
+    {
+        return $this->planta;
+    }
+
+    /**
+     * @param int $planta
+     * @return Grupo
+     */
+    public function setPlanta($planta)
+    {
+        $this->planta = $planta;
+        return $this;
+    }
 }
